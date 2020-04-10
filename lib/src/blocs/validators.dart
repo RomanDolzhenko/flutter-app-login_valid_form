12ac3db20885bc6ac43708535d33a978f146bf42
Razor <rsadfsad@mal.ru>
2020-04-10 09:57:23 +0300

    first commit

---
+++ lib/src/blocs/validators.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+
+class Validators {
+  final validateEmail =
+      StreamTransformer<String, String>.fromHandlers(handleData: (email, sink) {
+    if (email.contains('@'))
+      sink.add(email);
+    else
+      sink.addError('Enter valid email');
+  });
+
+  final validatePassword = StreamTransformer<String, String>.fromHandlers(
+      handleData: (password, sink) {
+    if (password.length > 4)
+      sink.add(password);
+    else
+      sink.addError('Password to short');
+  });
+}
